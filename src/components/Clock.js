25b9b373cedbc77698402c06307d4de999621898
adambailey <arb5433@gmail.com>
2021-03-23 11:22:05 -0400

    “finished project”

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,51 +1,45 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      time: new Date()
-    };
-  }
+const Clock = () => {
+  const [time, setTime] = useState(new Date());
+  
+  const tick = () => {
+  console.log('updated');
+  setTime( new Date() );
+  };
 
-  componentDidMount() {
-    this.interval = setInterval(this.tick, 1000);
-  }
+  useEffect(() => {
+    console.log('useEffect')
+    const interval = setInterval(tick, 1000);
+    return () => {
+      clearInterval(interval);
+    }
+  },[]);
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
 
-  tick = () => {
-    this.setState({ time: new Date() });
-  }
+  hours = (hours < 10) ? `0${hours}` : hours;
+  minutes = (minutes < 10) ? `0${minutes}` : minutes;
+  seconds = (seconds < 10) ? `0${seconds}` : seconds;
 
-  render() {
-    let hours = this.state.time.getHours();
-    let minutes = this.state.time.getMinutes();
-    let seconds = this.state.time.getSeconds();
-
-    hours = (hours < 10) ? `0${hours}` : hours;
-    minutes = (minutes < 10) ? `0${minutes}` : minutes;
-    seconds = (seconds < 10) ? `0${seconds}` : seconds;
-
-    return (
-      <section>
-        <h1>Clock</h1>
-        <div className='clock'>
-          <p>
-            <span>
-              Time:
-            </span>
-            <span>
-              {hours}:{minutes}:{seconds} PDT
-            </span>
-          </p>
-          <p>Date: {this.state.time.toDateString()}</p>
-        </div>
-      </section>
-    );
-  }
-};
+  return (
+    <section>
+      <h1>Clock</h1>
+      <div className='clock'>
+        <p>
+          <span>
+            Time:
+          </span>
+          <span>
+            {hours}:{minutes}:{seconds} PDT
+          </span>
+        </p>
+        <p>Date: {time.toDateString()}</p>
+      </div>
+    </section>
+  );
+}
 
 export default Clock;
